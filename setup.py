0fb3f136585e4d3c102307aba7afa57a434b1051
zkwiatkowska <z.kwiatkowska7@gmail.com>
2021-04-19 02:20:28 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""Library setup."""
+import setuptools
+from setuptools import setup
+
+setup(
+    name='storir',
+    version='0.1.0',
+    description='Python package with code described in https://arxiv.org/abs/2008.07231.',
+    url='https://github.com/SRPOL-AUI/storir',
+    license='MIT License',
+    packages=setuptools.find_packages(),
+    install_requires=['numpy>=1.19'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License ',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.8'
+    ],
+)
